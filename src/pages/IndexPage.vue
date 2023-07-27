af0ed915e330afbfa6911b805e26f52549c42f16
Anirudha Jadhav <anirudhasj441@gmail.com>
2023-07-27 22:41:56 +0530

    dev: change indent size from 2 to 4 dev: add bootstrap icons set dev: add projects section

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,143 +1,194 @@
 <template>
-  <q-page class="q-pb-md" :style-fn="setHeight">
-    <q-scroll-area class="q-pr-md" :class="$q.platform.is.mobile ? 'q-pl-md' : ''" style="height: 100%;">
-      <q-card flat class="full-width" style="border-radius: 10px;">
-        <q-card-section class="q-px-lg">
-          <div class="row q-col-gutter-x-lg items-center">
-            <div class="col-xs-12 col-md-6 flex flex-center" :style="height_style">
-              <div class="info">
-                <div class="" style="font-size: 1rem;">Hello I am,</div>
-                <div class="text-h2 text-bold text-primary">Anirudha Jadhav</div>
-                <div class="bio">
-                  {{ bio }}
+    <q-page class="q-pb-md" :style-fn="setHeight">
+        <q-scroll-area class="q-pr-md" :class="$q.platform.is.mobile ? 'q-pl-md' : ''" style="height: 100%;">
+            <q-card flat class="full-width" style="border-radius: 10px;">
+                <q-card-section class="q-px-lg">
+                    <div class="row q-col-gutter-x-lg items-center">
+                        <div class="col-xs-12 col-md-6 flex flex-center" :style="height_style">
+                            <div class="info">
+                                <div class="" style="font-size: 1rem;">Hello I am,</div>
+                                <div class="text-h2 text-bold text-primary">Anirudha Jadhav</div>
+                                <div class="bio">
+                                    {{ bio }}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-md-6 flex flex-center" :class="$q.platform.is.mobile ? 'order-first' : ''"
+                            :style="height_style">
+                            <q-img src="/banner_image.svg" style="width: 100%;" :ratio="1"></q-img>
+                        </div>
+                    </div>
+                </q-card-section>
+            </q-card>
+
+            <!-- projects section -->
+            <div v-if="projects.length > 0">
+                <div class="text-h5 text-bold q-my-md q-mt-lg">Projects</div>
+                <div class="row q-col-gutter-md">
+                    <div class="col-xs-6 col-md-3" v-for="project in projects" :key="project.skill">
+                        <q-card flat>
+                            <q-img :src="'/api' + project.img" style="width: 100%;" :ratio="16 / 9">
+
+                            </q-img>
+                            <!-- <q-card-section class="text-center">
+                            </q-card-section> -->
+                            <q-card-section>
+                                <div class=" flex items-center justify-between q-gutter-x-sm no-wrap">
+                                    <div class="text-h6">{{ project.title }}</div>
+                                    <q-space></q-space>
+                                    <a v-if="project.git_repo" :href="project.git_repo" class="me-2 project-link"
+                                        target="_blank" rel="noopener noreferrer">
+                                        <q-icon name="img:/github.svg" style="font-size: 1.5rem;"></q-icon>
+                                    </a>
+                                    <a v-if="project.link" :href="project.link" class="me-2 project-link" target="_blank"
+                                        rel="noopener noreferrer">
+                                        <q-icon name="bi-box-arrow-up-right"></q-icon>
+                                    </a>
+                                </div>
+                            </q-card-section>
+                        </q-card>
+                    </div>
                 </div>
-              </div>
             </div>
-            <div class="col-xs-12 col-md-6 flex flex-center" :class="$q.platform.is.mobile ? 'order-first' : ''"
-              :style="height_style">
-              <q-img src="/banner_image.svg" style="width: 100%;" :ratio="1"></q-img>
+
+            <!-- Frontend skill section -->
+            <div v-if="frontend_skills.length > 0">
+                <div class="text-h5 text-bold q-my-md q-mt-lg">Frontend</div>
+                <div class="row q-col-gutter-md">
+                    <div class="col-xs-6 col-md-3" v-for="skill in frontend_skills" :key="skill.skill">
+                        <q-card flat>
+                            <q-card-section class="text-center">
+                                <q-img :src="'/api' + skill.icon" style="width: 50px;" :ratio="1"></q-img>
+                            </q-card-section>
+                            <q-card-section>
+                                <div class="text-h6 text-center">{{ skill.skill }}</div>
+                            </q-card-section>
+                        </q-card>
+                    </div>
+                </div>
             </div>
-          </div>
-        </q-card-section>
-      </q-card>
 
-      <!-- Frontend skill section -->
-      <div class="text-h5 text-bold q-my-md q-mt-lg">Frontend</div>
-      <div class="row q-col-gutter-md">
-        <div class="col-xs-6 col-md-3" v-for="skill in frontend_skills" :key="skill.skill">
-          <q-card flat>
-            <q-card-section class="text-center">
-              <q-img :src="'/api' + skill.icon" style="width: 50px;" :ratio="1"></q-img>
-            </q-card-section>
-            <q-card-section>
-              <div class="text-h6 text-center">{{ skill.skill }}</div>
-            </q-card-section>
-          </q-card>
-        </div>
-      </div>
+            <!-- Backend Skill Section -->
+            <div v-if="backend_skills.length > 0">
+                <div class="text-h5 text-bold q-my-md q-mt-lg">Backend</div>
+                <div class="row q-col-gutter-md">
+                    <div class="col-xs-6 col-md-3" v-for="skill in backend_skills" :key="skill.skill">
+                        <q-card flat>
+                            <q-card-section class="text-center">
+                                <q-img :src="'/api' + skill.icon" style="width: 50px;" :ratio="1"></q-img>
+                            </q-card-section>
+                            <q-card-section>
+                                <div class="text-h6 text-center">{{ skill.skill }}</div>
+                            </q-card-section>
+                        </q-card>
+                    </div>
+                </div>
+            </div>
 
-      <!-- Backend Skill Section -->
-      <div class="text-h5 text-bold q-my-md q-mt-lg">Backend</div>
-      <div class="row q-col-gutter-md">
-        <div class="col-xs-6 col-md-3" v-for="skill in backend_skills" :key="skill.skill">
-          <q-card flat>
-            <q-card-section class="text-center">
-              <q-img :src="'/api' + skill.icon" style="width: 50px;" :ratio="1"></q-img>
-            </q-card-section>
-            <q-card-section>
-              <div class="text-h6 text-center">{{ skill.skill }}</div>
-            </q-card-section>
-          </q-card>
-        </div>
-      </div>
+            <!-- Database skill section -->
+            <div v-if="database_skills.length > 0">
+                <div class="text-h5 text-bold q-my-md q-mt-lg">Database</div>
+                <div class="row q-col-gutter-md">
+                    <div class="col-xs-6 col-md-3" v-for="skill in database_skills" :key="skill.skill">
+                        <q-card flat>
+                            <q-card-section class="text-center">
+                                <q-img :src="'/api' + skill.icon" style="width: 50px;" :ratio="1"></q-img>
+                            </q-card-section>
+                            <q-card-section>
+                                <div class="text-h6 text-center">{{ skill.skill }}</div>
+                            </q-card-section>
+                        </q-card>
+                    </div>
+                </div>
+            </div>
 
-      <!-- Database skill section -->
-      <div class="text-h5 text-bold q-my-md q-mt-lg">Database</div>
-      <div class="row q-col-gutter-md">
-        <div class="col-xs-6 col-md-3" v-for="skill in database_skills" :key="skill.skill">
-          <q-card flat>
-            <q-card-section class="text-center">
-              <q-img :src="'/api' + skill.icon" style="width: 50px;" :ratio="1"></q-img>
-            </q-card-section>
-            <q-card-section>
-              <div class="text-h6 text-center">{{ skill.skill }}</div>
-            </q-card-section>
-          </q-card>
-        </div>
-      </div>
-
-      <!-- Other skill section -->
-      <div class="text-h5 text-bold q-my-md q-mt-lg">Others</div>
-      <div class="row q-col-gutter-md">
-        <div class="col-xs-6 col-md-3" v-for="skill in other_skills" :key="skill.skill">
-          <q-card flat>
-            <q-card-section class="text-center">
-              <q-img :src="'/api' + skill.icon" style="width: 50px;" :ratio="1"></q-img>
-            </q-card-section>
-            <q-card-section>
-              <div class="text-h6 text-center">{{ skill.skill }}</div>
-            </q-card-section>
-          </q-card>
-        </div>
-      </div>
-      <!-- <div v-for="skill in frontend_skills" :key="skill.skill" class="text-h6">{{ skill.skill }}</div> -->
-    </q-scroll-area>
-  </q-page>
+            <!-- Other skill section -->
+            <div v-if="other_skills.length > 0">
+                <div class="text-h5 text-bold q-my-md q-mt-lg">Others</div>
+                <div class="row q-col-gutter-md">
+                    <div class="col-xs-6 col-md-3" v-for="skill in other_skills" :key="skill.skill">
+                        <q-card flat>
+                            <q-card-section class="text-center">
+                                <q-img :src="'/api' + skill.icon" style="width: 50px;" :ratio="1"></q-img>
+                            </q-card-section>
+                            <q-card-section>
+                                <div class="text-h6 text-center">{{ skill.skill }}</div>
+                            </q-card-section>
+                        </q-card>
+                    </div>
+                </div>
+            </div>
+            <!-- <div v-for="skill in frontend_skills" :key="skill.skill" class="text-h6">{{ skill.skill }}</div> -->
+        </q-scroll-area>
+    </q-page>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      bio: '',
-      skills: [],
-      frontend_skills: [],
-      backend_skills: [],
-      database_skills: [],
-      other_skills: [],
-      height_style: null
-    }
-  },
-  methods: {
-    setHeight: function (offset) {
-      let height_style = {
-        height: offset ? `calc(100vh - ${offset}px)` : '100vh'
-      }
-      this.height_style = height_style;
-      return height_style
+    data() {
+        return {
+            bio: '',
+            skills: [],
+            frontend_skills: [],
+            backend_skills: [],
+            database_skills: [],
+            other_skills: [],
+            projects: [],
+            height_style: null
+        }
     },
-    getProfile: function () {
-      let url = "/api/get_profile/";
-      const xhr = new XMLHttpRequest();
-      xhr.open("get", url);
-      xhr.onload = () => {
-        let response = JSON.parse(xhr.response);
-        console.log(response);
-        this.bio = response.data.bio;
-        this.skills = response.data.skills;
-        this.skills.forEach((skill) => {
-          switch (skill.category) {
-            case 'frontend':
-              this.frontend_skills.push(skill);
-              break;
-            case 'backend':
-              this.backend_skills.push(skill);
-              break
-            case 'database':
-              this.database_skills.push(skill);
-              break
-            default:
-              this.other_skills.push(skill);
-              break;
-          }
-        });
+    methods: {
+        setHeight: function (offset) {
+            let height_style = {
+                height: offset ? `calc(100vh - ${offset}px)` : '100vh'
+            }
+            this.height_style = height_style;
+            return height_style
+        },
+        getProfile: function () {
+            let url = "/api/get_profile/";
+            const xhr = new XMLHttpRequest();
+            xhr.open("get", url);
+            xhr.onload = () => {
+                let response = JSON.parse(xhr.response);
+                console.log(response);
+                this.bio = response.data.bio;
+                this.skills = response.data.skills;
+                this.skills.forEach((skill) => {
+                    switch (skill.category) {
+                        case 'frontend':
+                            this.frontend_skills.push(skill);
+                            break;
+                        case 'backend':
+                            this.backend_skills.push(skill);
+                            break
+                        case 'database':
+                            this.database_skills.push(skill);
+                            break
+                        default:
+                            this.other_skills.push(skill);
+                            break;
+                    }
+                });
 
-      }
-      xhr.send();
+            }
+            xhr.send();
+        },
+        getProjects: function () {
+            let url = "/api/get_projects/";
+            const xhr = new XMLHttpRequest();
+            xhr.open("get", url);
+            xhr.onload = () => {
+                let response = JSON.parse(xhr.response);
+                console.log(response);
+                this.projects = response.data;
+            }
+            xhr.send();
+        }
+    },
+    mounted() {
+        this.getProfile();
+        this.getProjects();
     }
-  },
-  mounted() {
-    this.getProfile();
-  }
 }
 
 //new file
